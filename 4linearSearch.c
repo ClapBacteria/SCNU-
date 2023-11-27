5a9c528e8f9f7d6742b029da5ee800a4646247f3
ClapBacteria <149381872+ClapBacteria@users.noreply.github.com>
2023-11-27 23:23:35 +0900

    Update 4linearSearch.c

--- 4linearSearch.c
+++ 4linearSearch.c
@@ -20,7 +20,7 @@ int main()
     int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     int size = sizeof(a) / sizeof(a[0]);
 
-    printf("프로그래머 박수균\n학번: 20204352\n학과: 컴퓨터공학과\n");
+    printf("프로그래머 박수균 \n 학번: 20204352\n학과: 컴퓨터공학과\n");
     printf("\n순차검색(sequentialsearch)\n");
 
     printf("\n검색 대상 자료\n");
