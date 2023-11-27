583933c7fa3c8d490ccbbdb6f86649811760ced3
ClapBacteria <149381872+ClapBacteria@users.noreply.github.com>
2023-11-27 23:25:30 +0900

    Update 5binarySearch.c

--- 5binarySearch.c
+++ 5binarySearch.c
@@ -31,13 +31,13 @@ int main(void) {
     printf("프로그래머 박수균 \n학번: 20204352 \n학과 : 컴퓨터공학과 \n");
     printf("\n이진 검색(Binary Search)\n");
 
-    printf("\n검색 대상 자료\n");
+    printf("\n검색 대상 자료 \n");
     for (i = 0; i < size; i++)
         printf("%d, ", a[i]);
 
-    printf("\n\n검색할 키를 입력하세요: ");
+    printf("\n\n검색할 키를 입력하세요 : ");
     scanf("%d", &key);
-    printf("\n%d를 검색\n", key);
+    printf("\n%d를 검색 \n", key);
 
     binarySearch(a, 0, size - 1, key);
 
